c7d562313b254dfe864f4127d7957aae824beb5e
tejasri1234 <tejasrimoole91@gmail.com>
2025-05-12 06:54:03 +0000

    emplyee form

--- app/project/webapp/controller/employee.controller.js
+++ app/project/webapp/controller/employee.controller.js
@@ -1,10 +1,57 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     "use strict";
-    return Controller.extend("project.controller.Employee", {
-        onInit: function() {
-            console.log("Employee Controller Initialized");
+
+    return Controller.extend("your.namespace.controller.employee", {
+        onInit: function () {
+            // Initialize a JSON model to store form data
+            var oData = {
+                name: "",
+                dob: "",
+                email: "",
+                phone: "",
+                qualification: "",
+                branch: "",
+                cgpa: "",
+                files: []
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel, "employee");
+        },
+
+        onSubmit: function () {
+            var oView = this.getView();
+            var oModel = oView.getModel("employee");
+            var oData = oModel.getData();
+
+            // Simple validation
+            if (!oData.name || !oData.email || !oData.phone || !oData.qualification) {
+                MessageToast.show("Please fill in all required fields!");
+                return;
+            }
+
+            // Log data for debugging
+            console.log("Employee Data Submitted:", oData);
+
+            // Simulating a backend call
+            MessageToast.show("Form submitted successfully!");
+        },
+
+        onFileUpload: function (oEvent) {
+            var oUploader = oEvent.getSource();
+            var aFiles = oUploader.getSelectedFiles();
+
+            if (aFiles.length === 0) {
+                MessageToast.show("Please select a file to upload.");
+                return;
+            }
+
+            var oModel = this.getView().getModel("employee");
+            oModel.setProperty("/files", aFiles);
+            MessageToast.show("Files uploaded successfully!");
         }
     });
 });
