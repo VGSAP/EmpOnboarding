e33e45df8bf6a7d0aaa087fec32a43ab3cd12085
Tejasri Moole <tejasrimoole91@gmail.com>
2025-05-13 08:38:34 +0000

    validating users

--- srv/srv.js
+++ srv/srv.js
@@ -1,19 +1,15 @@
 const cds = require('@sap/cds');
+const {User} = cds.entities('emp.onboard');
 
 module.exports = async (srv) => {
-    srv.before('CREATE', 'Users', async (req) => {
-        const { EMAIL, PASSWORD, MOBILENUMBER } = req.data;
-        
-        if (!EMAIL.match(/^\S+@\S+\.\S+$/)) {
-            req.error(400, "Invalid email format!");
-        }
-        
-        if (PASSWORD.length < 8) {
-            req.error(400, "Password must be at least 8 characters long!");
-        }
-
-        if (!MOBILENUMBER.match(/^[0-9]{10}$/)) {
-            req.error(400, "Invalid mobile number!");
-        }
+    srv.on('CREATE', 'UserDetails', async (req) => {
+        const tx = cds.transaction(req);
+         try {
+            const res = await tx.run(INSERT.into(User).entries(req.data));
+            return res;
+        } catch (err) {
+        console.error('Error during CREATE:', err);
+        req.error(500, 'Failed to insert user details');
+     }
     });
 }
