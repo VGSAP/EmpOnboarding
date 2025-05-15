ac1168a820a351117ab6639925303f597ef18283
Akhilesh gangishetty <akhileshgangi10@gmail.com>
2025-05-15 08:43:24 +0000

    buttons in hr

--- app/project/webapp/controller/hr.controller.js
+++ app/project/webapp/controller/hr.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-  ], (BaseController) => {
-    "use strict";
-  
-    return BaseController.extend("project.controller.hr", {
-        onInit(){
-  
-        }
-    });
+  "sap/ui/core/mvc/Controller"
+], function (Controller) {
+  "use strict";
+
+  return Controller.extend("project.controller.hr", {
+      onInit: function () {
+          this._router = sap.ui.core.UIComponent.getRouterFor(this);
+      },
+
+      onNavTohome: function () {
+          this._router.navTo("home");
+      },
+
+      onNavToEmployee: function () {
+          this._router.navTo("employee");
+      },
+
+      onNavToVerify: function () {
+          this._router.navTo("verify");
+      }
   });
-  
+});
